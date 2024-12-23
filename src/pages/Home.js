a6a35a89f7f37c39e5690ee0559cfea2f108c7e6
Rachel Lee <rachelslee2713@gmail.com>
2024-12-23 16:28:54 -0500

    added styling for small screens

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import SocialMediaLinks from "../components/SocialMediaLinks";
 import Me from "../assets/me.JPG";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
 const Home = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  const boxStyle = {
+    borderRadius: 1,
+    bgcolor: "white",
+  };
+
   return (
-    <div className="max-w-full mx-auto px-4 py-8 flex justify-center">
-      <div className="w-1/2 h-3/4 rounded-lg border-solid border-2 border-slate-950">
-        <div className="p-8 flex items-center space-x-6">
-          {/* My picture */}
-          <div className="p-3 rounded-lg">
-            <img src={Me} className="w-96" alt="Me" />
-          </div>
-          {/* Words */}
-          <div className="w-1/2">
-            <div className="h-1/2">
-              <h1 className="text-2xl font-semibold mb-2">Hi, I'm Rachel</h1>
-              <p className="my-2">
-                welcome to my website!
-                <br />
-              </p>
-              <p className="text-blue-600">
-                <b>tl;dr</b> I'm a bs/ms student at uchicago studying math & CS.
-                i like to work on cool projects, be adventurous, & ponder.
-              </p>
-            </div>
-            <div className="h-1/4 flex place-items-end">
-              <SocialMediaLinks />
-            </div>
-          </div>
+    <div className="w-screen h-screen mx-auto px-4 py-8 flex justify-center">
+      <div className="p-6 w-fit h-fit flex items-center lg:w-3/4 xl:w-1/2 justify-center md:space-x-6 border-solid border border-slate-950 rounded-lg">
+        {/* My picture */}
+        <div className="hidden md:block">
+          <img src={Me} className="w-80" alt="Me" />
+        </div>
+        {/* Words */}
+        <div className="w-full h-full md:w-1/2">
+          <h1 className="text-2xl font-semibold mb-2">Hi, I'm Rachel</h1>
+          <p className="my-2 text-xl">
+            welcome to my website!
+            <br />
+          </p>
+          <p className="text-blue-600 text-xl hidden lg:block">
+            <b>tl;dr</b> I'm a bs/ms student at uchicago studying math & CS. i
+            like to work on cool projects, be adventurous, & ponder.
+          </p>
+          <p className="text-blue-600 text-xl block lg:hidden">
+            <button type="button" onClick={toggleModal}>
+              <b>tl;dr</b>
+            </button>
+          </p>
+          <SocialMediaLinks />
         </div>
       </div>
+      <Modal
+        open={modal}
+        onClose={toggleModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={boxStyle} className="absolute top-1/4 left-1/4 w-1/2 h-fit">
+          <p className="m-5 text-sm md:text-2xl md:m-10 text-blue-600">
+            <b>tl;dr</b> I'm a bs/ms student at uchicago studying math & CS. i
+            like to work on cool projects, be adventurous, & ponder.
+          </p>
+        </Box>
+      </Modal>
     </div>
   );
 };
